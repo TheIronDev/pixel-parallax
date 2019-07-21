3534225cbfadde26de684a0e43ff443253555887
Tyler Stark <theirondeveloper@gmail.com>
2019-07-21 13:53:10 -0500

    Move rendering into renderer

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 class RenderObject {
   /**
-   * @param {!HTMLCanvasElement} ctx
    * @param {number} xStep
    * @param {number} yStep
    * @param {number} xMax
@@ -9,8 +8,7 @@ class RenderObject {
    * @param {number} size
    * @param {number} speed
    */
-  constructor(ctx, xStep, yStep, xMax, yMax, size, speed) {
-    this.ctx = ctx;
+  constructor(xStep, yStep, xMax, yMax, size, speed) {
     this.xStep = xStep;
     this.yStep = yStep;
     this.xMax = xMax;
@@ -27,26 +25,12 @@ class RenderObject {
 
   /** @abstract */
   move() {}
+
+  /** @abstract */
+  static generate() {}
 }
 
 class Cloud extends RenderObject {
-  draw(width, height) {
-    this.ctx.lineWidth = this.size;
-
-    height = height/2;
-    width = width/2;
-
-    const x = (width / this.xMax) * this.xStep;
-    const y = (height / this.yMax) * this.yStep;
-    this.ctx.beginPath();
-    this.ctx.moveTo(x, y);
-    this.ctx.lineTo(x + 18 * (width / this.xMax), y);
-
-    this.ctx.moveTo(x + 5 * (width / this.xMax), y - (height / this.yMax));
-    this.ctx.lineTo(x + 9 * (width / this.xMax), y - (height / this.yMax));
-    this.ctx.stroke();
-  }
-
   move() {
     this.xStep -= this.speed;
 
@@ -58,24 +42,21 @@ class Cloud extends RenderObject {
   reset() {
     this.xStep = this.xMax + this.size;
   }
+
+  static generate() {
+    const xMax = 1000;
+    const x = ~~(Math.random() * xMax);
+
+    const yMax = 100;
+    const y = Math.random() * yMax/2;
+
+    const size = 10 + Math.random() * 10;
+    const speed = Math.random() * 2;
+    return new Cloud(xMax + ~~(x /2), ~~y, xMax, yMax, ~~size, speed/3);
+  }
 }
 
 class Building extends RenderObject {
-  draw(width, height) {
-    width = width/2;
-
-    const x = (width / this.xMax) * this.xStep;
-    const buildingBase = height / 3;
-    this.ctx.fillStyle = '#555';
-    this.ctx.beginPath();
-    this.ctx.fillRect(x, buildingBase, 20, this.size * -1);
-    this.ctx.fillStyle = '#fff';
-    for (let i = 0; i < this.size/10 - 1; i ++) {
-      this.ctx.fillRect(x + 2.5, buildingBase - 2.5 - (i * 10), 5, -5);
-      this.ctx.fillRect(x + 12.5, buildingBase - 2.5 - (i * 10), 5, -5);
-    }
-  }
-
   move() {
     this.xStep -= this.speed;
 
@@ -87,12 +68,110 @@ class Building extends RenderObject {
   reset() {
     this.xStep = this.xMax;
   }
+
+  static generate(count) {
+    const xMax = 1000;
+    const x = count * 100;
+
+    const yMax = 100;
+    const y = Math.random() * yMax/2;
+
+    const size = 50 + ~~(Math.random() * 100);
+    return new Building(xMax + ~~(x /2), ~~y, xMax, yMax, ~~size, 1/4);
+  }
+}
+
+class Renderer {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.height = 0;
+    this.width = 0;
+    this.devicePixelRatio = 1;
+  }
+
+  clear() {
+    this.ctx.clearRect(0,0, this.ctx.canvas.width, this.ctx.canvas.height);
+  }
+
+  drawBuildings(buildings) {
+    this.ctx.fillStyle = '#666';
+    buildings.forEach(building => {
+      building.move();
+      this.drawBuilding(building);
+    });
+  }
+
+  drawBuilding(building) {
+    let width = this.width/this.devicePixelRatio;
+
+    const x = (width / building.xMax) * building.xStep;
+    const buildingBase = this.height / 3;
+    this.ctx.fillStyle = '#555';
+    this.ctx.beginPath();
+    this.ctx.fillRect(x, buildingBase, 20, building.size * -1);
+    this.ctx.fillStyle = '#fff';
+    for (let i = 0; i < building.size/10 - 1; i ++) {
+      this.ctx.fillRect(x + 2.5, buildingBase - 2.5 - (i * 10), 5, -5);
+      this.ctx.fillRect(x + 12.5, buildingBase - 2.5 - (i * 10), 5, -5);
+    }
+  }
+
+  drawClouds(clouds) {
+    this.ctx.fillStyle = '#fff';
+    this.ctx.strokeStyle = '#fff';
+    this.ctx.lineCap = 'round';
+    clouds.forEach(cloud => {
+      cloud.move();
+      this.drawCloud(cloud);
+    });
+  }
+
+  drawCloud(cloud) {
+    this.ctx.lineWidth = cloud.size;
+
+    let height = this.height/this.devicePixelRatio;
+    let width = this.width/this.devicePixelRatio;
+
+    const x = (width / cloud.xMax) * cloud.xStep;
+    const y = (height / cloud.yMax) * cloud.yStep;
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, y);
+    this.ctx.lineTo(x + 18 * (width / cloud.xMax), y);
+
+    this.ctx.moveTo(x + 5 * (width / cloud.xMax), y - (height / cloud.yMax));
+    this.ctx.lineTo(x + 9 * (width / cloud.xMax), y - (height / cloud.yMax));
+    this.ctx.stroke();
+  }
+
+  drawGround() {
+    this.ctx.fillStyle = '#2d7229';
+    this.ctx.beginPath();
+    this.ctx.fillRect(0, this.height / 3, this.width, this.height);
+  }
+
+  draw(buildings, clouds) {
+    this.clear();
+    this.drawClouds(clouds);
+    this.drawBuildings(buildings);
+    this.drawGround();
+  }
+
+  setDimentions(height, width, devicePixelRatio) {
+
+    this.height = height;
+    this.width = width;
+    this.devicePixelRatio = devicePixelRatio;
+
+    this.ctx.canvas.height = innerHeight * devicePixelRatio;
+    this.ctx.canvas.width = innerWidth * devicePixelRatio;
+    this.ctx.scale(devicePixelRatio, devicePixelRatio);
+  }
 }
 
 class App {
-  constructor(window, ctx, clouds, buildings) {
+  constructor(window, renderer, clouds, buildings) {
     this.window = window;
-    this.ctx = ctx;
+    this.renderer = renderer;
     this.clouds = clouds;
     this.buildings = buildings;
 
@@ -100,39 +179,8 @@ class App {
     this.width = 0;
   }
 
-  drawBuildings_() {
-    this.ctx.fillStyle = '#666';
-    this.buildings.forEach(building => {
-      building.move();
-      building.draw(this.width, this.height);
-    });
-  }
-
-  drawClouds_() {
-    this.ctx.fillStyle = '#fff';
-    this.ctx.strokeStyle = '#fff';
-    this.ctx.lineCap = 'round';
-    this.clouds.forEach(cloud => {
-      cloud.move();
-      cloud.draw(this.width, this.height);
-    });
-  }
-
-  drawGround_() {
-    this.ctx.fillStyle = '#2d7229';
-    this.ctx.beginPath();
-    this.ctx.fillRect(0, this.height / 3, this.width, this.height);
-  }
-
-  clearCtx() {
-    this.ctx.clearRect(0,0, this.ctx.canvas.width, this.ctx.canvas.height);
-  }
-
   draw() {
-    this.clearCtx();
-    this.drawClouds_();
-    this.drawBuildings_();
-    this.drawGround_();
+    this.renderer.draw(this.buildings, this.clouds);
   }
 
   renderLoop() {
@@ -142,12 +190,7 @@ class App {
 
   resize() {
     const {innerHeight, innerWidth, devicePixelRatio} = this.window;
-    this.height = innerHeight;
-    this.width = innerWidth;
-
-    this.ctx.canvas.height = innerHeight * devicePixelRatio;
-    this.ctx.canvas.width = innerWidth * devicePixelRatio;
-    this.ctx.scale(devicePixelRatio, devicePixelRatio);
+    this.renderer.setDimentions(innerHeight, innerWidth, devicePixelRatio);
   }
 
   start() {
@@ -160,15 +203,7 @@ class App {
 function initClouds(ctx, count) {
   const clouds = [];
   while (count--) {
-    const xMax = 1000;
-    const x = ~~(Math.random() * xMax);
-
-    const yMax = 100;
-    const y = Math.random() * yMax/2;
-
-    const size = 10 + Math.random() * 10;
-    const speed = Math.random() * 2;
-    clouds.push(new Cloud(ctx, xMax + ~~(x /2), ~~y, xMax, yMax, ~~size, ~~speed + 1));
+    clouds.push(Cloud.generate());
   }
   return clouds;
 }
@@ -176,14 +211,7 @@ function initClouds(ctx, count) {
 function initBuildings(ctx, count) {
   const buildings = [];
   while (count--) {
-    const xMax = 1000;
-    const x = count * 100;
-
-    const yMax = 100;
-    const y = Math.random() * yMax/2;
-
-    const size = 50 + ~~(Math.random() * 100);
-    buildings.push(new Building(ctx, xMax + ~~(x /2), ~~y, xMax, yMax, ~~size, 1));
+    buildings.push(Building.generate(count));
   }
   return buildings;
 }
@@ -193,8 +221,10 @@ const ctx = canvas.getContext('2d');
 const clouds = initClouds(ctx, 10);
 const buildings = initBuildings(ctx, 10);
 
+const renderr = new Renderer(ctx);
+
 // Create our "app"
-const app = new App(window, ctx, clouds, buildings);
+const app = new App(window, renderr, clouds, buildings);
 
 // Start the render loop
 app.start();
